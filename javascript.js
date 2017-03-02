7f43fcda936dfadac1e53e56b4b2526a69a93a36
katelyj <kjohnston@stuy.edu>
2017-03-02 14:44:03 -0500

    first work

---
+++ javascript.js
@@ -0,0 +1,18 @@
+var dvd = new Image();
+dvd.src = "dvd.png";
+
+
+var addCircle = function(e) {
+    c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+};
+
+var addDVD = funtion(e) {
+};
+
+var stop = function(e) {
+};
+
+
+circleButton.addEventListener("click", addCircle);
+dvdButton.addEventListener("click", addDVD);
+stopButton.addEventListener("click", stop);
