ca2e72009e80000a8831be840f75b9e35f0ced4e
katelyj <kjohnston@stuy.edu>
2017-03-05 16:47:57 -0500

    taco

--- javascript.js
+++ javascript.js
@@ -3,47 +3,95 @@ dvd.src = "dvd.png";
 
 var rid = 0;
 
+
 var addCircle = function(e) {
-    
-    window.cancelAnimationFrame(rid);
+
+    stop();
     
     var rad = 0;
-    var x = 250; // 500/2
-    var y = 250; // 500/2
-
-    var growing = true;
-    var speed = 5;
+    var speed = 1;
+    var x = s.clientWidth / 2;
+    var y = s.clientHeight / 2;
 
     var draw = function() {
+
+	clear();
     
-	c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 	c.setAttribute("cx", x);
 	c.setAttribute("cy", y);
 	c.setAttribute("r", rad);
 	c.setAttribute("fill", "blue");
-	c.setAttribute("id", "cir");
 	s.appendChild(c);
 
 	rad += speed;
 
-	if( rad == y || rad == 0 )
-	    speed = -speed;
+	if ( rad == y || rad == 0 ) {
+	    speed *= -1;
+	}
 
 	rid = window.requestAnimationFrame(draw);
 
-    };
+    }
 
     draw();
     
 };
 
-var addDVD = funtion(e) {
+var addDVD = function(e) {
+
+    stop();
+
+    var x = Math.random() * (s.clientWidth - 50);
+    var y = Math.random() * (s.clientWidth - 50);
+    var x1 = 1;
+    var y1 = 1;
+    
+    if( Math.round( Math.random() ) == 0 ) {
+	x1 *= -1;
+    }
+    
+    if( Math.round( Math.random() ) == 0 ) {
+	y1 *= -1;
+    }
+
+    var move = function() {
+
+	clear();
+
+	var d = document.createElementNS("http://www.w3.org/2000/svg", "image");
+	d.setAttribute("href", "dvd.jpeg");
+	d.setAttribute("x", x);
+	d.setAttribute("y", y);
+	s.appendChild(d);
+
+	x += x1;
+	y += y1;
+
+	if ( x + 50 > s.clientWidth || x < 0 ) {
+	    x1 *= -1;
+	}
+	
+	if ( y + 50 > s.clientHeight || y < 0 ) {
+	    y1 *= -1;
+	}
+
+	rid = window.requestAnimationFrame(move);
+	
+    }
+
+    move();
     
 };
 
 var stop = function(e) {
-    
+    window.cancelAnimationFrame(rid);
 };
+
+var clear = function(e) {
+    s.innerHTML = '';
+};
+
 
 circleButton.addEventListener("click", addCircle);
 dvdButton.addEventListener("click", addDVD);
